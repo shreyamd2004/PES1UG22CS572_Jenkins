af7f4b7c2fb36a97b1a9d9d9cd03cf139e618358
shreyamd2004 <shreyadevaraju@gmail.com>
2025-03-13 16:41:56 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "Shreya M D PES1UG22CS572";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
